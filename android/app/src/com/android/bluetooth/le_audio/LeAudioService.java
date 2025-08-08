miyu-xu/packages-modules-Bluetooth

[LE Broadcast] filter out null metadata while getAllBroadcastMetadata
SystemUI crashed while parse for null metadata. To avoid this issue,
LeAudioService should filter out null metadata while return for
getAllBroadcastMetadata API.

Change-Id: If9e61d533edd4a8c21e684107c7d1be31c1416f8

android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
@@ -1546,6 +1546,7 @@ public class LeAudioService extends ProfileService {
     public List<BluetoothLeBroadcastMetadata> getAllBroadcastMetadata() {
         return mBroadcastDescriptors.values().stream()
                 .map(s -> s.mMetadata)
+                .filter(s -> s != null)
                 .collect(Collectors.toList());
     }
 

HamelinPorts/android_packages_modules_Bluetooth

Bug: 409888761
Test: atest LeAudioBroadcastServiceTest
Flag: EXEMPT, day to day fix, unit test added.
(cherry picked from https://android-review.git.corp.google.com/c/platform/packages/modules/Bluetooth/+/3496157)
Change-Id: I9f1cb1ca6eafb03b0c56ecedf88ddb2c9a6189cd

android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
@@ -1610,6 +1610,7 @@ public class LeAudioService extends ConnectableProfile {
     public List<BluetoothLeBroadcastMetadata> getAllBroadcastMetadata() {
         return mBroadcastDescriptors.values().stream()
                 .map(s -> s.mMetadata)
+                .filter(m -> m != null)
                 .collect(Collectors.toList());
     }
 